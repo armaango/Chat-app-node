ba6ee5e8550917c2215d7c266190d074eb633317
armaango <armaan.goyal@gmail.com>
2017-02-20 16:40:00 -0800

    created generate message utility

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to server');
-
-    socket.emit('createMessage',{
-        to:'jennny',
-        text:'heyy'
-    })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
@@ -14,19 +16,23 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log('New User connected');
+socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
-    socket.emit('newMessage',{
-        from:'ammy',
-        text:'heyy',
-        createdAt:1233232
-    });
+socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'))
+
+
+socket.on('createMessage', (message)=>{
+    console.log('createMessage',message);
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    // socket.broadcast.emit('newMessage', {
+    //     from:message.text,
+    //     text:messa.text,
+    //     createdAt: new Date().getTime()
+    // });
+});
 
 socket.on('disconnect',()=>{
     console.log('Client disconnected');
-});
-
-socket.on('createMessage', (newMessage)=>{
-    console.log('createMessage',newMessage);
 });
 
 });
