5612867f9be80e8e8120fc193ed7b79f7baf2123
armaango <armaan.goyal@gmail.com>
2017-02-21 00:13:49 -0800

    added mustache.js and added message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,25 +10,31 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}:${formattedTime} -  ${message.text}`);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt:formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank"> My current location</a>');
-    li.text(`${message.from}: ${formattedTime}`);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
-})
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt:formattedTime
+    });
+
+    jQuery('#messages').append(html);
+});
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
-var messageTextBox = jQuery('[name=message]');
+    var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextBox.val()
@@ -42,7 +48,7 @@ locationButton.on('click', function (e) {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
-    locationButton.attr('disabled','disabled').text('Sending Location...');
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text('Send Location');
